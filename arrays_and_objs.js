8cad41d3f7cbffabb843024e13aa91c4e763fdf8
Veron Sundaram <veronsundaram13@gmail.com>
2022-09-20 15:28:12 +0100

    first commit

---
+++ arrays_and_objs.js
@@ -0,0 +1,14 @@
+//console.log("hey");
+
+const groceries = ["milk", "bread", "eggs", "apple"];
+
+
+groceries.push("dog food");
+
+
+groceries.pop();
+
+let length = groceries.length;
+
+console.log(groceries);
+console.log(length);
---
+++ hello_world.js
@@ -0,0 +1,7 @@
+myName = "Veron";
+
+temperature = 11;
+
+console.log("Hello "+ myName);
+
+console.log("The temperature is "+ temperature  + " degrees today. ");
